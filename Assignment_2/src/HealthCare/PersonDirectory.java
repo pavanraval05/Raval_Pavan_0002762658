a52b87a50ac9db490bf7d2010696183bf420013c
pavan996066 <pavan996066@gmail.com>
2022-10-30 16:04:09 -0400

    17th Commit
    Project all done.

--- Assignment_2/src/HealthCare/PersonDirectory.java
+++ Assignment_2/src/HealthCare/PersonDirectory.java
@@ -9,5 +9,38 @@ package HealthCare;
  * @author pavan
  */
 public class PersonDirectory {
+     private String perNme;
+    private String perComm;
+    private int personIiD;
+
+    public String getPerNme() {
+        return perNme;
+    }
+
+    public void setPerNme(String perNme) {
+        this.perNme = perNme;
+    }
+
+    public String getPerComm() {
+        return perComm;
+    }
+
+    public void setPerComm(String perComm) {
+        this.perComm = perComm;
+    }
+
+    public int getPersonIiD() {
+        return personIiD;
+    }
+
+    public void setPersonIiD(int personIiD) {
+        this.personIiD = personIiD;
+    }
+
+    public PersonDirectory(String perNme, String perComm, int personIiD) {
+        this.perNme = perNme;
+        this.perComm = perComm;
+        this.personIiD = personIiD;
+    }
     
 }
