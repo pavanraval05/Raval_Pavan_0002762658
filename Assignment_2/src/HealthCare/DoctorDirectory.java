d79fcb9f80d9546e3999e641e5f53635931113c4
pavan996066 <pavan996066@gmail.com>
2022-10-31 05:40:58 -0400

    40
    class diagram

--- Assignment_2/src/HealthCare/DoctorDirectory.java
+++ Assignment_2/src/HealthCare/DoctorDirectory.java
@@ -9,5 +9,57 @@ package HealthCare;
  * @author pavan
  */
 public class DoctorDirectory {
-    
+    public String username12;
+   public String password1;
+   public String nam;
+   public String gendr;
+   public String department1;
+
+    public DoctorDirectory(String username12, String password1, String nam, String gendr, String department12) {
+        this.username12 = username12;
+        this.password1 = password1;
+        this.nam = nam;
+        this.gendr = gendr;
+        this.department1 = department12;
+    }
+
+    public String getUsername12() {
+        return username12;
+    }
+
+    public void setUsername12(String username12) {
+        this.username12 = username12;
+    }
+
+    public String getPassword1() {
+        return password1;
+    }
+
+    public void setPassword1(String password1) {
+        this.password1 = password1;
+    }
+
+    public String getNam() {
+        return nam;
+    }
+
+    public void setNam(String nam) {
+        this.nam = nam;
+    }
+
+    public String getGendr() {
+        return gendr;
+    }
+
+    public void setGendr(String gendr) {
+        this.gendr = gendr;
+    }
+
+    public String getDepartment12() {
+        return department1;
+    }
+
+    public void setDepartment12(String department12) {
+        this.department1 = department12;
+    }
 }
